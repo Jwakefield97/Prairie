3032d142ef6940f67dcfd070340b27b14cc1ce69
Jwakefield97 <jacobstevenwakefield@gmail.com>
2018-11-07 10:37:24 -0600

    add to comment

--- lib/http/http.go
+++ lib/http/http.go
@@ -3,4 +3,6 @@ package http
 /*
 	This file will contain http related structs and methods. Primarily structs to deal with 
 	headers and http reponse statuses. 
+
+	for headers see: https://en.wikipedia.org/wiki/List_of_HTTP_header_fields
 */
