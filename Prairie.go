f84e8a7881dd943c2c5e797fdb2550d4127ec53e
Jwakefield97 <jacobstevenwakefield@gmail.com>
2018-11-07 11:55:33 -0600

    update comment

--- Prairie.go
+++ Prairie.go
@@ -9,7 +9,7 @@ package prairie
 	TODO: add the Request and Response structs as parameters to RequestCallback
 	TODO: add a Response struct to NewPrairieInstance so the user can set a default template for reponses. If on is not passed in 
 	then a default Reponse struct should be created that autofills certain headers.
-	TODO: add lib for dealing with JSON 
+	TODO: add lib for dealing with JSON: https://golang.org/pkg/compress/gzip/
 	TODO: add lib to gzip reponse bodies 
 	TODO: add an in memory session store. 
 */
