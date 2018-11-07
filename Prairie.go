c27c7d285169b848e0ffb1d3c00daefc7d7d9983
jake <jacobstevenwakefield@gmail.com>
2018-11-06 20:02:37 -0600

    add initial files

---
+++ Prairie.go
@@ -0,0 +1,14 @@
+package prairie
+
+import (
+	"fmt"
+)
+
+/*
+	This is the entry point to the framework. All helper functions/libraries are placed in the folder ./lib.
+	MAKE SURE THIS PROJECT IS LOCATED IN YOUR SRC FOLDER OF GO PATH UNDER THE FOLDER "prairie"
+*/
+
+func PrintFromFramework() {
+	fmt.Println("printing from the framework entry point.")
+}
---
+++ test/Server.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"prairie"
+)
+
+/*
+	This is a test file to implement the framework and test it as is developed.
+*/
+
+func main() {
+	fmt.Println("This is the a driver to test the framework.")
+	prairie.PrintFromFramework()
+}
