2002ddb62264067d2ebfd6bfa392bf72dac0f896
jwakefield97 <jacobstevenwakefield@gmail.com>
2018-11-07 15:36:31 -0600

    update test example

--- test/Server.go
+++ test/Server.go
@@ -8,22 +8,17 @@ import (
 /*
 	This is a test file to implement the framework and test it as is developed.
 */
-
-func indexCallback() {
-	fmt.Println("***Inside of the index callback***")
-}
-
-func uploadsCallback() {
-	fmt.Println("***Inside of the uploads callback***")
-}
-
 func main() {
 	fmt.Println("This is the a driver to test the framework.")
 	app := prairie.NewPrairieInstance("127.0.0.1", 3000)
 
-	app.Get("/index", indexCallback)
+	app.Get("/index", func() {
+		fmt.Println("***Inside of the index callback***")
+	})
 
-	app.Post("/uploads", uploadsCallback)
+	app.Post("/uploads", func() {
+		fmt.Println("***Inside of the uploads callback***")
+	})
 
 	app.Start()
 }
