1d5cf8be3ce3c927f1329af90ecb5ff37b92d1c9
jwakefield97 <jacobstevenwakefield@gmail.com>
2018-11-07 14:30:08 -0600

    add link to routine safe map

--- lib/utils/sessionUtils.go
+++ lib/utils/sessionUtils.go
@@ -2,5 +2,6 @@ package utils
 
 /*
 	This file will hold all structs/functions to deal with the server's session. It should use mutex's when 
-	dealing with the session data structure because it will accessed/modified accross multiple go routines.
+	dealing with the session data structure because it will accessed/modified accross multiple go routines. 
+	Potentially look at using the Map from the sync package (https://golang.org/pkg/sync/).
 */
