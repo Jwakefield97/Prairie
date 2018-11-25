e77ec63b7613c9e7d0798380fcaef92f4f414195
jake <jacobstevenwakefield@gmail.com>
2018-11-25 00:48:27 -0600

    remove unused file

--- lib/utils/sessionUtils.go
+++
@@ -1,7 +0,0 @@
-package utils
-
-/*
-	This file will hold all structs/functions to deal with the server's session. It should use mutex's when 
-	dealing with the session data structure because it will accessed/modified accross multiple go routines. 
-	Potentially look at using the Map from the sync package (https://golang.org/pkg/sync/).
-*/
