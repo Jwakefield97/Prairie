69ed17d1119619c9b6ce15cfa138e6113fc683a7
jake <jacobstevenwakefield@gmail.com>
2018-11-29 13:50:12 -0600

    add basic document server

---
+++ docs/Server.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Jwakefield97/prairie"
+)
+
+/*
+	This is a test file to implement the framework and test it as is developed.
+*/
+func main() {
+	fmt.Println("This is the a driver to test the framework.")
+	app := prairie.NewPrairieInstance("localhost", 2000)
+	app.ResourceDir = "resources"
+	app.TemplateDir = "templates"
+
+	app.Get("/", func(routeObj *prairie.RouteObject) {
+		routeObj.Response.File = "templates/prairie.html"
+	})
+	app.Get("/prairie/", func(routeObj *prairie.RouteObject) {
+		routeObj.Response.File = "templates/prairie.html"
+	})
+	app.Get("/http/", func(routeObj *prairie.RouteObject) {
+		routeObj.Response.File = "templates/http.html"
+	})
+	app.Get("/utils/", func(routeObj *prairie.RouteObject) {
+		routeObj.Response.File = "templates/utils.html"
+	})
+
+	app.Start()
+}
--- getHtmlDoc.sh
+++ getHtmlDoc.sh
@@ -1,20 +1,17 @@
 #!/bin/sh
 #make sure godocs server is running. Use command (godoc -html :6060)
-mkdir docs
-mkdir docs/css
-mkdir docs/js
 
 #html
-godoc -url http://localhost:6060/pkg/github.com/Jwakefield97/prairie/ > docs/prairie.html
-godoc -url http://localhost:6060/pkg/github.com/Jwakefield97/prairie/lib/utils/ > docs/utils.html
-godoc -url http://localhost:6060/pkg/github.com/Jwakefield97/prairie/lib/http/ > docs/http.html
+godoc -url http://localhost:6060/pkg/github.com/Jwakefield97/prairie/ > docs/templates/prairie.html
+godoc -url http://localhost:6060/pkg/github.com/Jwakefield97/prairie/lib/utils/ > docs/templates/utils.html
+godoc -url http://localhost:6060/pkg/github.com/Jwakefield97/prairie/lib/http/ > docs/templates/http.html
 
 #css
-godoc -url http://localhost:6060/lib/godoc/style.css > docs/css/style.css
-godoc -url http://localhost:6060/lib/godoc/jquery.treeview.css > docs/css/jquery.treeview.css
+godoc -url http://localhost:6060/lib/godoc/style.css > docs/resources/css/style.css
+godoc -url http://localhost:6060/lib/godoc/jquery.treeview.css > docs/resources/css/jquery.treeview.css
 
 #js
-godoc -url http://localhost:6060/lib/godoc/jquery.js > docs/js/jquery.js
-godoc -url http://localhost:6060/lib/godoc/jquery.treeview.js > docs/js/jquery.treeview.js
-godoc -url http://localhost:6060/lib/godoc/jquery.treeview.edit.js > docs/js/jquery.treeview.edit.js
-godoc -url http://localhost:6060/lib/godoc/godocs.js > docs/js/godocs.js
+godoc -url http://localhost:6060/lib/godoc/jquery.js > docs/resources/js/jquery.js
+godoc -url http://localhost:6060/lib/godoc/jquery.treeview.js > docs/resources/js/jquery.treeview.js
+godoc -url http://localhost:6060/lib/godoc/jquery.treeview.edit.js > docs/resources/js/jquery.treeview.edit.js
+godoc -url http://localhost:6060/lib/godoc/godocs.js > docs/resources/js/godocs.js
